fbfa4fba7709d697951c73e3fe1468f773c28385
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-05-17 16:02:38 +0200

    append functions

--- solution.js
+++ solution.js
@@ -11,17 +11,12 @@ for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
     element.setAttribute('data-preview', id)
 
     element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${image}"
-        />
-        
+        <img class="preview__image" src="${image}" />
         <div class="preview__info">
             <h3 class="preview__title">${title}</h3>
             <div class="preview__author">${authors[author]}</div>
         </div>
     `
-
     starting.appendChild(element)
 }
 
@@ -47,11 +42,13 @@ const selectors = {
 
 document.querySelector(selectors.listItems).appendChild(starting)
 
-const genreHtml = document.createDocumentFragment()
-const firstGenreElement = document.createElement('option')
-firstGenreElement.value = 'any'
-firstGenreElement.innerText = 'All Genres'
-genreHtml.appendChild(firstGenreElement)
+
+function appendGenre(){
+    const genreHtml = document.createDocumentFragment()
+    const firstGenreElement = document.createElement('option')
+    firstGenreElement.value = 'any'
+    firstGenreElement.innerText = 'All Genres'
+    genreHtml.appendChild(firstGenreElement)
 
 for (const [id, name] of Object.entries(genres)) {
     const element = document.createElement('option')
@@ -59,11 +56,9 @@ for (const [id, name] of Object.entries(genres)) {
     element.innerText = name
     genreHtml.appendChild(element)
 }
-
-function append(){
 document.querySelector(selectors.searchGenre).appendChild(genreHtml)
 }
-append();
+appendGenre();
 
 function appendAuthors(){
     const authorsHtml = document.createDocumentFragment()
@@ -77,7 +72,7 @@ for (const [id, name] of Object.entries(authors)) {
     element.value = id
     element.innerText = name
     authorsHtml.appendChild(element)
-document.querySelector('[data-search-authors]').appendChild(authorsHtml)
+    document.querySelector(selectors.searchAuthors).appendChild(authorsHtml)
 }
 }
 appendAuthors();
@@ -110,8 +105,7 @@ const updateListButton2 = () =>{
     <span class="list__remaining"> (${(matches.length - (page * BOOKS_PER_PAGE)) > 0 ? (matches.length - (page * BOOKS_PER_PAGE)) : 0})</span>
 `
 }
-       
-
+     updateListButton2();  
 
 document.querySelector('[data-search-cancel]').addEventListener('click', () => {
     document.querySelector('[data-search-overlay]').open = false
@@ -191,22 +185,17 @@ document.querySelector('[data-search-form]').addEventListener('submit', (event) 
         element.setAttribute('data-preview', id)
     
         element.innerHTML = `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
-            
+            <img class="preview__image" src="${image}"/>
             <div class="preview__info">
                 <h3 class="preview__title">${title}</h3>
                 <div class="preview__author">${authors[author]}</div>
             </div>
         `
-
         newItems.appendChild(element)
     }
 
     document.querySelector('[data-list-items]').appendChild(newItems)
-     updateListButton2();
+     
 
     window.scrollTo({top: 0, behavior: 'smooth'});
     document.querySelector('[data-search-overlay]').open = false
@@ -221,17 +210,12 @@ document.querySelector('[data-list-button]').addEventListener('click', () => {
         element.setAttribute('data-preview', id)
     
         element.innerHTML = `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
-            
+            <img class="preview__image" src="${image}"/>
             <div class="preview__info">
                 <h3 class="preview__title">${title}</h3>
                 <div class="preview__author">${authors[author]}</div>
             </div>
         `
-
         fragment.appendChild(element)
     }
 
